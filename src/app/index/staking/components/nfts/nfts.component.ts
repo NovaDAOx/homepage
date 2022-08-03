a68e21e635f2fbde9672d2cd3f0433688ff46c55
Pawan Kumar <pawan@lapits.com>
2022-08-03 12:42:40 +0530

    fixed some ui issues

--- src/app/index/staking/components/nfts/nfts.component.ts
+++ src/app/index/staking/components/nfts/nfts.component.ts
@@ -1,24 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nfts',
-  templateUrl: './nfts.component.html',
-  styleUrls: ['./nfts.component.scss']
+  selector: "app-nfts",
+  templateUrl: "./nfts.component.html",
+  styleUrls: ["./nfts.component.scss"],
 })
 export class NftsComponent implements OnInit {
-@Input() nft:String[]
-async checkAll(bx) {
-  var cbs =await document.getElementsByTagName('input');
-  for(var i=0; i < cbs.length; i++) {
-    if(cbs[i].type == 'checkbox') {
-      cbs[i].checked = bx.checked;
-    }
-  }
-}
-  constructor() { }
+  @Input() nft: {};
+
+  constructor() {}
 
   ngOnInit(): void {
-    const cheklist = document.getElementById('checkl')
+    const cheklist = document.getElementById("checkl");
   }
 
+  async checkAll(bx) {
+    var cbs = await document.getElementsByTagName("input");
+    for (var i = 0; i < cbs.length; i++) {
+      if (cbs[i].type == "checkbox") {
+        cbs[i].checked = bx.checked;
+      }
+    }
+  }
 }
--- src/app/index/staking/stake.component.ts
+++ src/app/index/staking/stake.component.ts
@@ -1,248 +1,210 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import {Moralis} from 'moralis'
-import { MoralisService } from 'src/app/services/moralis.service';
-import { MetamaskService } from 'src/app/services/metamask.service';
-import { Web3Service } from 'src/app/services/web3.service';
-import {EtherService} from 'src/app/services/ether.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { Moralis } from "moralis";
+import { MoralisService } from "src/app/services/moralis.service";
+import { MetamaskService } from "src/app/services/metamask.service";
+import { Web3Service } from "src/app/services/web3.service";
+import { EtherService } from "src/app/services/ether.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
-import { NgxSpinnerService } from 'ngx-spinner';
-import { NftService } from 'src/app/services/nft.service';
-import { MetamaskComponent } from 'src/app/dashboard/metamask/metamask.component';
+import { NgxSpinnerService } from "ngx-spinner";
+import { NftService } from "src/app/services/nft.service";
+import { MetamaskComponent } from "src/app/dashboard/metamask/metamask.component";
 import {
   MatDialog,
   MatDialogConfig,
   MatDialogRef,
-} from '@angular/material/dialog';
-import { Router } from '@angular/router';
-
+} from "@angular/material/dialog";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-stake',
-  templateUrl: './stake.component.html',
-  styleUrls: ['./stake.component.scss'],
-  encapsulation:ViewEncapsulation.None,
+  selector: "app-stake",
+  templateUrl: "./stake.component.html",
+  styleUrls: ["./stake.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
-
- export class StakeComponent implements OnInit {
- 
+export class StakeComponent implements OnInit {
   // constructor(private moralisservice:MoralisService) { }
-list:String[]
-loadFirstTime: boolean = true;
-isMetamask:boolean = true ;
+  list: String[];
+  loadFirstTime: boolean = true;
+  isMetamask: boolean = true;
 
-public userOb = this.moralisservice.observeUser();
+  STAKEbtn: any = null;
+  NFTbtn: any = null;
+  loading2: any = null;
+  loading1: any = null;
+  approvebtn: any = null;
+  Selectbtn: any = null;
+  Deselectbtn: any = null;
 
+  public userOb = this.moralisservice.observeUser();
 
-constructor(
-  private router: Router,
-  public dialog: MatDialog,
-  private metaMaskService: MetamaskService,
-  private moralisservice:MoralisService,
-  private web3Service: Web3Service,
-  private etherService: EtherService,
-  private snack: MatSnackBar,
-  private spinner: NgxSpinnerService
-  
+  dialogConfig = new MatDialogConfig();
+  "dialogueReference": MatDialogRef<MetamaskComponent>;
+  isConnected: any = null;
 
-)
- {}
- STAKEbtn:any = null;
- NFTbtn: any = null;
- loading2:any = null;
- loading1:any = null;
- approvebtn:any = null;
- Selectbtn:any = null;
- Deselectbtn:any = null;
- async select_all()
- {
-    this.Deselectbtn = 'show';
-    this.Selectbtn = null;
-    const allElements = Array.from(
-      document.querySelectorAll('input[type=checkbox]')
-    );
-    allElements.forEach(element => {
-      element.setAttribute('checked', 'checked');
-    });
- } 
- async deselect_all()
- {
-    this.Selectbtn = 'show';
-    this.Deselectbtn = null;
-    const allElements = Array.from(
-      document.querySelectorAll('input[type=checkbox]')
-    );
-    allElements.forEach(element => {
-      element.removeAttribute('checked');
-    });    
- }
- async interact()
-{
-  this.etherService.setApproval().then(data => {
+  NFTtoadx: any = null;
 
+  checkuser: any = null;
 
-    if(data)
-    {
+  loadingFlag = false;
 
-
-      const co = document.getElementById('stakingsection').style
-      co.backgroundColor = '#200028';
-      co.pointerEvents = 'auto';
-      co.opacity = '1'
-      this.NFTbtn = null;
-      this.loading2 = null;
-      
-      this.STAKEbtn = 'show'
-      this.approvebtn = null;
-      
-    }
-    else
-    {
-      this.STAKEbtn = null;
-      this.NFTbtn = 'show';
-      this.loading2 = 'show';
-      this.approvebtn = null;
-
-    
-    }
-    
-    return data
-    
-  })
-}
-
-
-async approveall()
-{
-  
-  const userAddress = localStorage.getItem('walletId')
-  if(userAddress)
-  {
-    this.allNFT();
-    this.loading1 = 'show';
-    
-    const co  = await document.getElementById('stakingsection').style
-    co.backgroundColor = "gray"
-    co.pointerEvents = 'none';
-    co.opacity = '0.7'      
-  }
-}
-  allNFT()
- {
-  
-    this.moralisservice.getnfts().then(data => {
-    
-    this.list=data
-    
-    this.NFTbtn='show'
-    if(data)
-    {
-      this.loading1 = null;
-    }
-    if(data.length == 0)
-    {
-      this.NFTtoadx="notoadx collection found"
-      this.NFTbtn=null;
-      
-    }
-    
-    if(data.length != 0)
-    {
-      this.approvebtn = 'show'
-    }
-   
-    
-  })
-
-
-}
-dialogConfig = new MatDialogConfig();
-'dialogueReference': MatDialogRef<MetamaskComponent>;
-isConnected: any = null;
-
-NFTtoadx: any =null;
-
-
-checkuser:any = null;
-
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    private metaMaskService: MetamaskService,
+    private moralisservice: MoralisService,
+    private web3Service: Web3Service,
+    private etherService: EtherService,
+    private snack: MatSnackBar,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
-   this.approveall()
-    
-    // if (history.state.routedFrom == 'stake') {
-    //   location.href = 'home#roadmap';
-    // }
-    if (!window.localStorage.getItem('logout')) {
-      window.localStorage.setItem('logout', 'false');
+    this.approveall();
+
+    if (!window.localStorage.getItem("logout")) {
+      window.localStorage.setItem("logout", "false");
     }
     this.metaMaskService.accountChanged.subscribe((data) => {
       if (
         this.loadFirstTime == false &&
         data &&
-        window.localStorage.getItem('logout') == 'false'
+        window.localStorage.getItem("logout") == "false"
       ) {
         this.isConnected = null;
-       
-        this.NFTbtn= null;
+
+        this.NFTbtn = null;
         this.checkIfUserWalletConnected();
       } else if (
         this.loadFirstTime == true &&
         data &&
-        window.localStorage.getItem('logout') == 'false'
+        window.localStorage.getItem("logout") == "false"
       ) {
         this.loadFirstTime = false;
       }
     });
 
     this.metaMaskService.firstTimeMetamaskConnect.subscribe((data: any) => {
-      if (data === 'firstTimeMetamaskConnect') {
+      if (data === "firstTimeMetamaskConnect") {
         this.isConnected = null;
         this.NFTbtn = null;
         this.checkIfUserWalletConnected();
       }
     });
 
-    if (window.localStorage.getItem('logout') == 'false') {
+    if (window.localStorage.getItem("logout") == "false") {
       this.checkIfUserWalletConnected();
     }
-
-   
-    
-    
   }
+
+  async select_all() {
+    this.Deselectbtn = "show";
+    this.Selectbtn = null;
+    const allElements = Array.from(
+      document.querySelectorAll("input[type=checkbox]")
+    );
+    allElements.forEach((element) => {
+      element.setAttribute("checked", "checked");
+    });
+  }
+
+  async deselect_all() {
+    this.Selectbtn = "show";
+    this.Deselectbtn = null;
+    const allElements = Array.from(
+      document.querySelectorAll("input[type=checkbox]")
+    );
+    allElements.forEach((element) => {
+      element.removeAttribute("checked");
+    });
+  }
+
+  async interact() {
+    this.etherService.setApproval().then((data) => {
+      if (data) {
+        const co = document.getElementById("stakingsection").style;
+        co.backgroundColor = "#200028";
+        co.pointerEvents = "auto";
+        co.opacity = "1";
+        this.NFTbtn = null;
+        this.loading2 = null;
+
+        this.STAKEbtn = "show";
+        this.approvebtn = null;
+      } else {
+        this.STAKEbtn = null;
+        this.NFTbtn = "show";
+        this.loading2 = "show";
+        this.approvebtn = null;
+      }
+
+      return data;
+    });
+  }
+
+  async approveall() {
+    const userAddress = localStorage.getItem("walletId");
+    if (userAddress) {
+      this.allNFT();
+      this.loading1 = "show";
+
+      const co = await document.getElementById("stakingsection").style;
+      co.backgroundColor = "gray";
+      co.pointerEvents = "none";
+      co.opacity = "0.7";
+    }
+  }
+
+  allNFT() {
+    this.moralisservice.getnfts().then((data) => {
+      this.loadingFlag = true;
+      this.list = data;
+
+      console.log('data',data)
+
+      this.NFTbtn = "show";
+      if (data) {
+        this.loading1 = null;
+      }
+      if (data.length == 0) {
+        this.NFTtoadx = "notoadx collection found";
+        this.NFTbtn = null;
+      }
+
+      if (data.length != 0) {
+        this.approvebtn = "show";
+      }
+    });
+  }
+
   async checkIfUserWalletConnected() {
     this.isConnected = await this.metaMaskService.isMetaMaskConnected();
     if (!this.isConnected) {
       this.isMetamask = false;
-      localStorage.setItem('logout', 'true');
-      localStorage.removeItem('walletId');
+      localStorage.setItem("logout", "true");
+      localStorage.removeItem("walletId");
     } else {
-      localStorage.setItem('walletId', this.isConnected);
-      this.moralisservice.startMoralis().subscribe(() => console.log('Started Moralis'));
-     
-      this.allNFT();
+      localStorage.setItem("walletId", this.isConnected);
+      this.moralisservice
+        .startMoralis()
+        .subscribe(() => console.log("Started Moralis"));
 
+      this.allNFT();
     }
   }
 
-
   showMetamaskContent() {
     this.dialogueReference = this.dialog.open(MetamaskComponent, {
-      panelClass: 'custom-modalbox',
+      panelClass: "custom-modalbox",
       disableClose: true,
       data: (this.dialogConfig.data = {
         metamaskContent: true,
       }),
     });
-    this.dialogueReference.afterClosed().subscribe((result) => {
-    
-    });
+    this.dialogueReference.afterClosed().subscribe((result) => {});
   }
 
-
-  
-
   changeScrollbar() {
-    this.router.navigate(['/home'], { state: { routedFrom: 'stake' } });
+    this.router.navigate(["/home"], { state: { routedFrom: "stake" } });
   }
 }
